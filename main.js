78ca502fbf3845d02550aa139df4b36ca5d93e46
JayanAXHF <73993003+JayanAXHF@users.noreply.github.com>
2021-08-10 16:59:29 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,1 +1,21 @@
 // js
+
+var songs = ["music1.mp3", "music2.mp3"];
+
+function preload() {
+    loadSong(songs[1]);
+    loadSong(songs[2]);
+}
+
+function setup() {
+    canvas = createCanvas(600, 500);
+    canvas.center();
+
+    video = createCapture(VIDEO);
+    video.hide();
+
+}
+
+function draw() {
+    image(video, 0, 0, 600, 500);
+}
